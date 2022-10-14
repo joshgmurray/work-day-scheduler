ac2d1f7338b74645e4a1a382f72848d8b25dd90a
Josh Murray <joshmurray@Joshs-MBP.lan>
2022-10-14 12:40:46 -0700

    added current hour and past events

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,51 @@
 $(document).ready(function () {
-  var hours = [];
-  var nowHour = new Date().getHours();
+    var hours = [];
 
-  for (let index = 0; index < 24; index++) {
-    var element = index + (index < 12 ? "AM" : "PM");
-    hours.push(element);
-  }
-
-  console.log("hours ===", hours);
-
-  function generateHoursTable() {
-    var str = "<table><body >";
-      
-    for (let index = 0; index < hours.length; index++) {
-      const element = hours[index];
-      str +=
-        "<tr>" +
-        "<th width='10%'>" + element + "</th>" +
-        "<td width='80%' colspan='4' class='stage-current'>Welcome</td>" +
-        "<td width='10%' class='save-button'>save</td>" +
-        "</tr>";
+    for (let index = 0; index < 24; index++) {
+        var element = index < 12 ? index + "AM" : (index - 12) + "PM";
+        hours.push(element);
     }
 
-    str += "</body></table>"
-    $('#tableBody').append(str);
-  }
-  
-  generateHoursTable();
+    console.log("hours ===", hours);
+
+    function generateHoursTable() {
+        var str = "<table><body >";
+
+        for (let index = 0; index < hours.length; index++) {
+            const element = hours[index];
+            var middel = "<td id='" + element + "' width='80%' colspan='4' class='stage stage-current'><p id='p" + element + "'>welcome</p></td>"
+            var nowHour = new Date().getHours();
+            if (nowHour == (element.includes("PM") ? parseInt(element) + 12 : parseInt(element))) {
+                middel = "<td id='" + element + "'width='80%' colspan='4' class='stage stage-current-hour'>Current hour</td>"
+            }
+            if (nowHour > (element.includes("PM") ? parseInt(element) + 12 : parseInt(element))) {
+                middel = "<td id='" + element + "'width='80%' colspan='4' class='stage stage-old'>event that already happend</td>"
+            }
+            str +=
+                "<tr>" +
+                "<th width='10%'>" + element + "</th>" +
+                middel +
+                "<td width='10%' class='save-button'><button name='" + element + "'>save</button></td>" +
+                "</tr>";
+        }
+
+        str += "</body></table>"
+        $('#tableBody').html(str);
+    }
+
+    generateHoursTable();
+
+    setInterval(function() {
+        $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }, 1000);
+
+    $("button").click(function(e) {
+        console.log('button click ===', e.target.name)
+    })
+
+    $("th,td.stage").click(function(e) {
+        console.log('welcome click ===', e.target.id)
+        var selector = '#' + e.target.id;
+        $(selector).html('<input type="text" id="i' + e.target.id + '" />')
+    })
 });
